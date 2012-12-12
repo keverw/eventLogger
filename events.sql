46bb6725e3938f104b851ae234226e9fd47481c0
Kevin Whitman <keverw92@gmail.com>
2012-12-12 14:18:43 -0500

    added schema sql file

---
+++ events.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `events` (
+  `uuid` varchar(255) NOT NULL DEFAULT '',
+  `data` text NOT NULL,
+  `category` varchar(255) NOT NULL DEFAULT '',
+  `sortCode` varchar(255) NOT NULL DEFAULT '',
+  `logLevel` int(1) NOT NULL,
+  `unixTimeStamp` bigint(22) NOT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
